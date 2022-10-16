1bf3c582f69a2e80853bf975fd4de3108197c213
Janus Milojevic <janus.milojevic@gmail.com>
2022-10-16 18:29:05 +0200

    player movement implementation

---
+++ Rouge-Ritual/Player.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+export (int) var MAX_SPEED = 250
+export (int) var ACCELERATION = 20
+export (int) var FRICTION = 20
+export (int) var HOLD_SPEED = 150
+export (bool) var HOLDING_ITEM = false
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		get_node("Raven_Sprite").set_flip_h(false)
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		get_node("Raven_Sprite").set_flip_h(true)
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+func _physics_process(_delta):
+	get_input()
+	if HOLDING_ITEM:
+		velocity = velocity.normalized() * HOLD_SPEED
+	else:
+		velocity = velocity.normalized() * MAX_SPEED
+	velocity = move_and_slide(velocity)
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		if collision.collider.name.begins_with("Boss"):
+			print ("collided with", collision.collider.name)
+			apply_knockback()
+	
+
+
+func apply_knockback():
+	pass
+func apply_frozen_ground():
+	#friction
+	pass
+func apply_damage():
+	# apply damage in case of
+	# interaction with boss
+	# contact with the wall at certain speed
+	# contact with spider attack
+	pass
