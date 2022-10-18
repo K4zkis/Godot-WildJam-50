ad14fba036c8034eae73803a0574f9fdd3bd348d
Janus Milojevic <janus.milojevic@gmail.com>
2022-10-18 19:16:24 +0200

    Menu Scene creation

---
+++ Rogue-Ritual/MainManu.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+export var mainGameScene : PackedScene
+export var OptionsScene : PackedScene
+export var CreditsScene : PackedScene
+
+
+func _on_Start_Button_button_up():
+	get_tree().change_scene(mainGameScene.resource_path)
+
+
+func _on_Options_Button_button_up():
+	get_tree().change_scene(OptionsScene.resource_path)
+
+
+func _on_Credits_Button_button_up():
+	get_tree().change_scene(CreditsScene.resource_path)
