4034972061c37fc90b9426e6e077882c3fe6f06c
vinhtieu <kebintieu@gmail.com>
2022-10-19 04:10:37 +0200

    temp remove  glow to title screen

--- Rogue-Ritual/Light2D.gd
+++ Rogue-Ritual/Light2D.gd
@@ -1,5 +1,6 @@
 extends Light2D
 
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -7,22 +8,9 @@ extends Light2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	energy1 = 0
-	uplight = true
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-var energy1 
-var uplight
-func _process(delta):
-	if(energy1 <= 0):
-		uplight = true
-	elif(energy1 >= 1): 
-		uplight = false
-	if(uplight):
-		energy1 = energy1 + delta/2
-	else:	
-		energy1 = energy1 - delta/4
-	self.energy = energy1	
-
-
+func _process(_delta):
+	self.position = get_parent().get_node("Player").position
