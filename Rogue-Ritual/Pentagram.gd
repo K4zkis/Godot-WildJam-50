ba9b83e6ad1b643d80f3c8d5375a60ee7b072453
vinhtieu <kebintieu@gmail.com>
2022-10-19 03:02:07 +0200

    added glowing pentagram

---
+++ Rogue-Ritual/Pentagram.gd
@@ -0,0 +1,27 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	energy = 0
+	uplight = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+var energy
+var uplight
+func _process(delta):
+	if(energy <= 0):
+		uplight = true
+	elif(energy >= 1): 
+		uplight = false
+	if(uplight):
+		energy = energy + delta
+	else:	
+		energy = energy - delta/2
+	get_node("Light2D").energy = energy	
