8af9d68bbaed84e5f30e318e1243007b9654cac0
Janus Milojevic <janus.milojevic@gmail.com>
2022-10-21 17:08:34 +0200

    setting up different states witha signal transfer as well as death reset to 5 health from title screen

---
+++ Rogue-Ritual/Base_state.gd
@@ -0,0 +1,83 @@
+extends KinematicBody2D
+
+
+export (int) var MAX_SPEED = 80
+
+export (int) var ACCELERATION = 20
+export (int) var FRICTION = 50
+#export (int) var HOLD_SPEED = 40
+#export (bool) var HOLDING_ITEM = false
+export (float) var INVINCIBILITY_DURATION = 1.1
+
+onready var stats = PlayerStats
+onready var hurtbox = $Hurtbox
+
+var velocity = Vector2()
+var state = Player_state
+
+enum {
+	Garlic_1,
+	Garlic_2,
+	Garlic_3,
+	Garlic_4,
+	Garlic_5,
+	Player_state
+}
+
+
+
+func _ready():
+	stats.connect("no_health", self, "_on_Stats_no_health")
+	
+	
+	
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		get_node("Raven_Sprite").set_flip_h(false)
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		get_node("Raven_Sprite").set_flip_h(true)
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+func _physics_process(_delta):
+	
+	match state:
+		Garlic_1:
+			pass
+		Garlic_2:
+			pass
+		Garlic_3:
+			pass
+		Garlic_4:
+			pass
+		Garlic_5:
+			pass
+		Player_state:
+			pass
+		
+	if velocity == Vector2(0,0):
+		$Raven_Sprite.animation = "Idle"
+	else:
+		$Raven_Sprite.animation = "Walking"
+	
+	velocity = move_and_slide(velocity)
+		
+func apply_frozen_ground():
+	#friction
+	pass
+	
+	
+func apply_damage():
+	# apply damage in case of
+	# interaction with boss
+	# contact with the wall at certain speed
+	# contact with spider attack
+	pass
+	
+	
+
--- Rogue-Ritual/Player.gd
+++ Rogue-Ritual/Player.gd
@@ -1,98 +1,36 @@
-extends KinematicBody2D
+extends "res://Base_state.gd"
 
-export (int) var MAX_SPEED = 80
-export (int) var KNOCKBACK_SPEED =400
-export (int) var ACCELERATION = 20
-export (int) var FRICTION = 50
-export (int) var HOLD_SPEED = 40
-export (bool) var HOLDING_ITEM = false
 export (bool) var STUN_ACTIVE = false
 export (float) var STUN_TIME = 0.7
-export (float) var INVINCIBILITY_DURATION = 1.1
-var velocity = Vector2()
-var counter = 0
-
-enum {
-	Garlic_1,
-	Garlic_2,
-	Garlic_3,
-	Garlic_4,
-	Garlic_5,
-	Player_state
-}
-
-
-var state = Player_state
-
-
+export (int) var KNOCKBACK_SPEED =400
 
 onready var timer = get_parent().get_parent().get_node("Timer")
-onready var stats = PlayerStats
-onready var hurtbox = $Hurtbox
+
+var counter = 0
 
 func _ready():
-	stats.connect("no_health", self, "queue_free")
 	timer.set_one_shot(STUN_TIME)
-	
-	
-	
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-		get_node("Raven_Sprite").set_flip_h(false)
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-		get_node("Raven_Sprite").set_flip_h(true)
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	PlayerStats.set_health(5)
 
 func _physics_process(_delta):
 	
-	match state:
-		Garlic_1:
-			pass
-		Garlic_2:
-			pass
-		Garlic_3:
-			pass
-		Garlic_4:
-			pass
-		Garlic_5:
-			pass
-		Player_state:
-			pass
-	
-	
-	
 	if STUN_ACTIVE == false:
 		get_input()
-		if HOLDING_ITEM:
-			velocity = velocity.normalized() * HOLD_SPEED
-		else:
-			velocity = velocity.normalized() * MAX_SPEED
+#		Legacy Code:
+#		if HOLDING_ITEM:
+#			velocity = velocity.normalized() * HOLD_SPEED
+#		else:
+		velocity = velocity.normalized() * MAX_SPEED
 	else:
 		var knockback_direction = Vector2()
 		knockback_direction = (self.global_transform.origin - get_parent().get_node("Boss").global_transform.origin).normalized()
 		apply_knockback(knockback_direction,_delta)
-		
-	
-	if velocity == Vector2(0,0):
-		$Raven_Sprite.animation = "Idle"
-	else:
-		$Raven_Sprite.animation = "Walking"
-	
-	velocity = move_and_slide(velocity)
-	
 	
 	for index in get_slide_count():
 		var collision = get_slide_collision(index)
 		if collision.collider.name.begins_with("Boss"):
 			print ("collided with ", collision.collider.name)
 			STUN_ACTIVE = true
-	
 
 
 func apply_knockback(direction,_delta):
@@ -104,20 +42,6 @@ func apply_knockback(direction,_delta):
 		#change the speed with each frame "delta" through decrease_speed_bezier() function
 		velocity = decrease_speed_bezier(_delta,direction,counter)
 	counter +=1
-	
-	
-func apply_frozen_ground():
-	#friction
-	pass
-	
-	
-func apply_damage():
-	# apply damage in case of
-	# interaction with boss
-	# contact with the wall at certain speed
-	# contact with spider attack
-	pass
-	
 	
 func decrease_speed_bezier(_delta,direction,count):
 	#this is in progress
@@ -136,6 +60,7 @@ func decrease_speed_bezier(_delta,direction,count):
 	return direction*new_speed
 
 
+
 func Timer_timeout():
 	STUN_ACTIVE=false
 	counter = 0
@@ -145,9 +70,12 @@ func Timer_timeout():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	hurtbox.start_invincibility(INVINCIBILITY_DURATION)
-	print (stats.health)
 
 
 func _on_Stats_no_health():
+	get_tree().change_scene("res://Menu.tscn")
 	print("Player Died")
-	pass # Replace with function body.
+
+
+func _on_First_found_object_object_pressed():
+	print("Object_pressed signal has been received")
