fbdf43075fb13f1d667b223975bb0244145c4092
Janus Milojevic <janus.milojevic@gmail.com>
2022-10-23 17:28:10 +0200

    some clean up

--- Rogue-Ritual/Doorlogic/Button.gd
+++ Rogue-Ritual/Doorlogic/Button.gd
@@ -8,10 +8,10 @@ var open_icon = preload("res://Objects/door_open.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_parent().get_parent().get_node("DoorNode/CollisionShape2D").set_disabled(false)
 
 
 func _on_Button_pressed():
-	var doorSprite = get_node("Closed")
+	var doorSprite = get_parent().get_parent().get_node("DoorNode/Closed")
 	doorSprite.set_texture(open_icon)
 	get_parent().get_parent().get_node("DoorNode/CollisionShape2D").set_disabled(true)
